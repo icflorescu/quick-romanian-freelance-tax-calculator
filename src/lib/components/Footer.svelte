887a25a73bbd7758f8369e699749db89741c3725
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-10-05 20:29:21 +0300

    Improve footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,6 +2,9 @@
   <hr />
   Realizat de
   <a href="https://github.com/icflorescu" target="_blank">Ionuț-Cristian Florescu</a>.
+  <br />
+  Dacă ești freelancer, te-ar putea interesa
+  <a href="https://swapp.ro" target="_blank">swapp.ro</a>.
 </div>
 
 <style lang="scss">
@@ -9,6 +12,7 @@
     color: darken($text-color, 20);
     text-align: center;
     font-size: 0.85em;
+    line-height: 1.66;
   }
 
   hr {
