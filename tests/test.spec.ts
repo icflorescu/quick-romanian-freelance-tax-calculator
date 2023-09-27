61098b840318a75db1ad4997fd154985c607314e
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-09-27 16:43:57 +0300

    Update deps

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected subtitle', async ({ page }) => {
   await page.goto('/');
-  expect(await page.textContent('.subtitle')).toBe('PFA în sistem real, anul 2023.');
+  expect(await page.title()).toBe('Taxe freelancing România - PFA 2023');
 });
